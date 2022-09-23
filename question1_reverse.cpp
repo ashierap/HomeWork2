f364fbeb97d6e0bf094693d4bdfa322558832b36
ashierap <112123390+ashierap@users.noreply.github.com>
2022-09-22 22:54:25 -0400

    Update question1_reverse.cpp
    First commit

--- question1_reverse.cpp
+++ question1_reverse.cpp
@@ -14,5 +14,24 @@ int main()
 {
  // use the following array of string for your program.
  string words[6] = {"object","oriented","programming","sciences","dedication","commitment"};
+ int array_size = (sizeof(words)/sizeof(words[0]));
+ string result_array[6];
 
+ for (int array_element = 0; array_element < array_size; array_element++)
+ {
+    string test = words[array_element];
+    int str_size = test.length();
+
+    string result = "";
+    for (int old_index = 0; old_index < str_size; old_index++)
+    {
+        char letter;
+        letter = test[(str_size - 1) - old_index];
+        result += letter;
+        result_array[array_element] = result;
+    }
+    cout << result_array[array_element] << " ";
+ }
+
+ return 0;
 }
