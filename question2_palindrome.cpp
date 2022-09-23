5ed928119fdfd3589d62763e50b7c59e94776938
ashierap <112123390+ashierap@users.noreply.github.com>
2022-09-22 21:35:40 -0400

    Update question2_palindrome.cpp
    First commit

--- question2_palindrome.cpp
+++ question2_palindrome.cpp
@@ -4,3 +4,32 @@ characters which reads the same backward as forward. For example, the
 given words are radar, level, rotor, kayak palindromes. Your program 
 will return, for example “Yes, the radar is a palindrome” or “No, 
 the word house is not a palindrome”. (10 pts) */
+
+#include <iostream>
+#include <string>
+
+
+using namespace std;
+
+int main()
+{
+    string pal;
+    string pal_check = "";
+
+    cout << " \n Enter a word to check if it is a palindrome: ";
+    cin >> pal;
+
+    int str_size = pal.length();
+    for (int old_index = 0; old_index < str_size; old_index++)
+    {
+        char letter;
+        letter = pal[(str_size - 1) - old_index];
+        pal_check += letter;
+    }
+
+    if (pal.compare(pal_check) == 0)
+        cout << " Yes, the word " << pal << " is a palindrome." << endl << " ";
+    else
+        cout << " No, the word " << pal << " is not a palindrome." << endl << " ";
+ return 0;
+}
